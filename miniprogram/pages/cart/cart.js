2c26b3d52c4da83fab2b5b3a1fa6e595024964ea
the third eye <623893018@qq.com>
2019-04-02 18:41:12 +0800

    update at 02

--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -6,134 +6,165 @@ const utils = require('../../utils/utils.js');
 
 create(store, {
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        orderList: '',
-        orderTotal: 0,
-        allSelected: false,
-        selected: false,
-        allTotal: 0,
-        count: 0
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    orderList: '',
+    orderTotal: 0,
+    allSelected: false,
+    selected: false,
+    allTotal: 0,
+    count: 0
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
-        this._retrieveCart();
-    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this._retrieveCart();
+  },
 
-    _retrieveCart() {
-        db.collection('cart').get().then(res => {
-            // 订单数量
-            this.update({
-                count: res.data.length
-            })
-            // 订单总价
-            res.data.forEach(item => {
-                this.update({
-                    allTotal: this.data.allTotal + item.total
-                })
-            })
-            this.store.data.orderList = res.data;
-            this.update();
-        })
-    },
-
-    onSelect(e) {
-        // 通过选中数量，确定全选状态
-        if(e.detail.value.length==this.data.count) {
-            this.update({
-                allSelected: true
-            })
-        } 
-        else if (e.detail.value.length == this.data.count-1){
-            this.update({
-                allSelected: false
-            })
-        }
-
-        // 更新总价
+  _retrieveCart() {
+    db.collection('cart').get().then(res => {
+      // 订单数量
+      this.update({
+        count: res.data.length
+      })
+      // 订单总价
+      res.data.forEach(item => {
         this.update({
-            orderTotal: utils.sum(e.detail.value)
+          allTotal: this.data.allTotal + item.total
         })
-    },
-    onCountsTotal(e) {
-        console.log(e)
-    },
+      })
+      this.store.data.orderList = res.data;
+      this.update();
 
-    // 通过value是否有值，确定全选状态
-    onSelectAll(e) {
-        if(e.detail.value.length==0){
-            this.update({
-                selected: false,
-                orderTotal: 0
-            })
-        }else{
-            this.update({
-                selected: true,
-                orderTotal: this.data.allTotal
-            })
-        }
-    },
+      wx.setStorage({
+        key: 'cart',
+        data: res.data
+      })
+    })
+  },
 
-    onDelete(e) {
-        db.collection('cart').doc(e.currentTarget.dataset.id).remove({
-            success: res => {
-                console.log(res)
-            }
-        })
-        this.update({
-            allTotal: 0
-        })
-        this._retrieveCart();
-    },
-
-    onReady: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
+  onCheckChange(e) {
+    // 通过选中数量，确定全选状态
+    if (e.detail.value.length == this.data.count) {
+      this.update({
+        allSelected: true
+      })
+    } else if (e.detail.value.length == this.data.count - 1) {
+      this.update({
+        allSelected: false
+      })
     }
+
+    // 更新总价
+    this.update({
+      orderTotal: 0
+    })
+
+    wx.getStorage({
+      key: 'cart',
+      success: res => {
+        console.log(res, 'st');
+        var array = res.data;
+        array.filter(item => e.detail.value.includes(item._id));
+        console.log(array, 'arr')
+      },
+      // https://www.hellojava.com/a/55126.html
+    })
+    // e.detail.value.forEach(id => {
+    //   db.collection('cart').doc(id).get().then(res => {
+    //     console.log(res)
+    //     // this.update({
+    //     //   orderTotal: this.data.orderTotal + res.data.total
+    //     // })
+    //     wx.setStorage({
+    //       key: 'cart',
+    //       data: res.data
+    //     })
+    //   })
+    // })
+  },
+  onCountsTotal(e) {
+    console.log(e)
+  },
+
+  // onChecked(e) {
+  //   console.log(e)
+  // },
+
+  // 通过value是否有值，确定全选状态
+  onSelectAll(e) {
+    if (e.detail.value.length == 0) {
+      this.update({
+        selected: false,
+        orderTotal: 0
+      })
+    } else {
+      this.update({
+        selected: true,
+        orderTotal: this.data.allTotal
+      })
+    }
+  },
+
+  onDelete(e) {
+    db.collection('cart').doc(e.currentTarget.dataset.id).remove({
+      success: res => {
+        console.log(res)
+      }
+    })
+    this.update({
+      allTotal: 0
+    })
+    this._retrieveCart();
+  },
+
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
 })
